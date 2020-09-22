e4bee524ad4b94f851687d3776f717bc102c0d01
mm51593 <mm51593@fer.hr>
2020-09-22 16:15:33 +0200

    Add command function

--- functionsBase.py
+++ functionsBase.py
@@ -7,3 +7,14 @@ class functionsBase:
     async def test(bot, message, *args):
         await bot.sendMessage(message, 'Test')
         return
+    
+    async def commands(bot, message, *args):
+        text = '```Commands: '
+        i = 0
+        for x in list(bot.commands.keys()):
+            if i != 0:
+                text += ', '
+            text += x
+            i += 1
+        text += '```'
+        await bot.sendMessage(message, text)
